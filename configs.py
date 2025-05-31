128f1706545cf715c12149680d1b48ae0a3ff60b
tgrobotic1000 <onezwinteam@gmail.com>
2025-05-31 13:45:52 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit 
+# Subscribe YouTube Channel For Amazing Bot 
+# Ask Doubt on telegram 
 
 
 from os import path, getenv
