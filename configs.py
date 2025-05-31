755ca5cf2bcc6e495ea10406655056bd60478592
tgrobotic1000 <onezwinteam@gmail.com>
2025-05-31 07:41:04 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,10 +1,11 @@
-import os
+# config.py
 
 class Config:
-    API_ID = int(os.getenv("25165349", "0"))
-    API_HASH = os.getenv("d5ea503a893548d7d492f91fc72e8ce9", "")
-    BOT_TOKEN = os.getenv("7729291712:AAEyuIgE7VLStp1tpZrKiEVrTFFjUujZJks", "")
-    CHID = os.getenv("-1002662027422", "")
-    SUDO = int(os.getenv("7439650239", "0"))
+    API_ID = 25165349
+    API_HASH = "d5ea503a893548d7d492f91fc72e8ce9"
+    BOT_TOKEN = "7729291712:AAEyuIgE7VLStp1tpZrKiEVrTFFjUujZJks"
+    CHID = "-1002662027422"
+    SUDO = 7439650239
     MONGO_URI = "mongodb+srv://tgrobotic:nRUEU81Zc3r5gMxK@cluster0.omkktyb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
 cfg = Config()
